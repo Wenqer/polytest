94af83c8174c642f5fb7ae6f65b9264bad8ea165
y.voloshchuk <wenqeer@gmail.com>
2014-08-12 17:09:38 +0300

    create data grid

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,6 +4,46 @@
   document.addEventListener('polymer-ready', function() {
     // Perform some behaviour
     console.log('Polymer is ready to rock!');
+    // var meta = document.getElementById('grid').$.table.meta;
+    // console.log(meta);
+    document.getElementById('grid').$.table.meta = [
+      {
+        name: 'name',
+        label: 'Имя',
+        type: 'string',
+        sortable: true,
+        searchable: true,
+        editable: true,
+        required: true
+      },
+      {
+        name: 'email',
+        label: 'Email',
+        type: 'string',
+        sortable: true,
+        searchable: true,
+        editable: true,
+        required: true
+      },
+      {
+        name: 'goodId',
+        label: 'Product ID',
+        type: 'string',
+        sortable: true,
+        searchable: true,
+        editable: false,
+        required: true
+      },
+      {
+        name: 'creationDate',
+        label: 'Creation Date',
+        type: 'date',
+        sortable: true,
+        searchable: true,
+        editable: false,
+        required: true
+      }
+    ];
   });
 
 // wrap document so it plays nice with other libraries
